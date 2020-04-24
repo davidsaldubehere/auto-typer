c8470f9fb5fa24a423a4803968ef0ad5ba8591b2
David Saldubehere <davesaldub@gmail.com>
2020-04-24 15:58:47 -0400

    Created a nicer UI

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import eel
-import pyautogui
+import pyautogui as py
 from pynput import mouse
+import time
 
 def getClickPos(x, y, button, pressed):
     print(x,y)
     eel.updateClick(x, y)
+    return False
+
 @eel.expose
 def startListener():
-    with mouse.Listener(
-        on_click=getClickPos) as listener:
+    with mouse.Listener(on_click=getClickPos) as listener:
             listener.join()
 
+@eel.expose
+def start(data, delay):
+    time.sleep(int(delay))
+    breakData = data.split("$")
+    print(breakData)
+    for i in range(len(breakData)):
+        if len(breakData[i])==1:
+            try:
+                time.sleep(int(breakData[i]))
+            
+            except Exception:
+                pass
+        
+        elif len(breakData[i])!=1:
+            py.typewrite(breakData[i], .1)
 
 eel.init('web')
 eel.start('index.html')
